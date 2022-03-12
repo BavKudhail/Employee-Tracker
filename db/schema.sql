c9edaaf6668eaea7e285d7a471a032372710c05d
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-03-12 19:13:44 +0000

    debugged logic for schema.sql;

--- db/schema.sql
+++ db/schema.sql
@@ -6,27 +6,22 @@ USE eTracker_db
 
 -- DEPARTMENT TABLE 
 CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+);
 
-
--- ROLE TABLE
 CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT 
+);
 
-)
 
-
--- EMPLOYEE TABLE 
 CREATE TABLE employee(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT,
-
-)
+    role_id INT,
+    manager_id INT
+);
